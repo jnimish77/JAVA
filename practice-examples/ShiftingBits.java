e2cbb0cfa15a8b881887cbec7795f058f22e2d4c
Nimish Joshi <jnimish77@users.noreply.github.com>
2016-04-07 23:04:36 -0500

    Create ShiftingBits.java
    Write a class named ShiftingBits, which has a field value (an int) and a few methods:
     A constructor that takes an int as the argument and assigns it to value
     A method rightShifts(int n) that shifts value to the right by n bits.
     A method leftShifts(int n) that shifts value to the left by n bits. Both rightShifts() and leftShifts()
    use signed shifting.
     A method negateAllBits() that negates all bits of value.
     A main method that reads in an integer, prints out its binary format, prints out its binary format
    after right shifting it 3 bits, prints out its binary format after left shifting it 3 bits, and prints out its
    binary format after negating all its bits. Note that value itself is not changed after each method, so
    all the methods are performed based on the original value of value.
    Two test examples:
    java ShiftingBits 123
    >The original value:
    >1111011
    >After right shift 3 bits:
    >1111
    >After left shift 3 bits:
    >1111011000
    >After negating all bits:
    >11111111111111111111111110000100
    java BitsOperations -5
    >The original value:
    >11111111111111111111111111111011
    >After right shift 3 bits:
    >11111111111111111111111111111111
    >After left shift 3 bits:
    >11111111111111111111111111011000
    >After negating all bits:
    >100
    Hint 1: You can safely assume that the first argument to the main method args[0] is guaranteed to be an
    integer. Use Integer.valueOf(String) to convert the String argument args[0] to an int.
    Hint 2: You can use Integer.toBinaryString(int) to generate the binary format for an int.

---
+++ practice-examples/ShiftingBits.java
@@ -0,0 +1,48 @@
+
+package shiftingbits;
+	import java.util.Scanner;
+
+	public class ShiftingBits {
+
+	    String s;
+	    int value;
+	    public ShiftingBits()
+	    {
+	      Scanner in = new Scanner(System.in);
+	      System.out.println("Enter a number ");
+	      s = in.nextLine();
+	      value = Integer.parseInt(s);
+	      // System.out.println("i: " + i);
+	      System.out.println("Binary value is " + Integer.toBinaryString(value));
+
+	    }
+	        
+	    public int rightShifts()
+	    {
+	        int a= value >> 3;
+	        System.out.println("After right shifting 3 bits " + Integer.toBinaryString(a));
+	        return 0;
+	    }
+	    
+	     public int leftShifts()
+	    {
+	        int b= value << 3;
+	        System.out.println("After left shifting 3 bits " + Integer.toBinaryString(b));
+	        return 0;
+	    }
+	    
+	     public int negateALLBits()
+	    {
+	        System.out.println("After negating all bits " + Integer.toBinaryString(~value));
+	        return 0;
+	    }      
+	    public static void main(String[] args)
+	    {    
+	       ShiftingBits SB = new ShiftingBits();
+	       SB.rightShifts();
+	       SB.leftShifts();
+	       SB.negateALLBits();    
+	    }   
+	}
+
+
